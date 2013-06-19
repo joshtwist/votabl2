199b40e22b3e452e6a4ee12dcf98e1c8f1dc0bd4
unknown <jtwist@jtwist-surfpro.redmond.corp.microsoft.com>
2013-06-19 11:41:35 -0700

    added blobs.js

--- service/shared/config.js
+++ service/shared/config.js
@@ -1,4 +1,5 @@
-exports.settings = {
-	storagekey = "",
-	storagethingy = "",
-}
+exports.storage = {
+	accountName = "azmob",
+	accountKey = "jup/1UBhKhYCAZu7zyCO6okigetuCco7OGc9ea31kU+nMImVk3Dk7GKh9xt5GMVBdYW+VYAi9z06KvOIvJhnlQ==",
+	imageContainer = "events"
+};
