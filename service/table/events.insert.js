e6d3dd78c58012f003cda6b5ca35ce1f578d5143
unknown <jtwist@jtwist-surfpro.redmond.corp.microsoft.com>
2013-06-19 15:04:37 -0700

    updated insert

--- service/table/events.insert.js
+++ service/table/events.insert.js
@@ -1,4 +1,13 @@
+var _r = require;
+require = function(path) {
+	try {  return _r(path);	}
+	catch (e) { return _r('../../app_data/config/scripts/table/' + path); }
+}
+
+var blobs = require('../blobs.js');
+
 function insert(item, user, request) {
     // todo - create an image URL for blob storage
+    console.log(blobs);
     request.execute();
 }
