6a9b3b0583fa37b9a30de398ff8fa779d462271a
windowsazure <windowsazure>
2013-06-21 03:57:28 +0000

    Committing update from request https://votabl2.scm.azure-mobile.net/scmvfs/service/api/monitorevent.js

--- service/api/monitorevent.js
+++ service/api/monitorevent.js
@@ -2,6 +2,7 @@ exports.post = function(request, response) {
     // Use "request.service" to access features of your mobile service, e.g.:
     //   var tables = request.service.tables;
     //   var push = request.service.push;
-
+    console.log(request.body);
+    console.log(request.data);
     response.send(200, "Hello World");
 };
