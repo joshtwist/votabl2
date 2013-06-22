9cb4b22efb0ae0b5d96a3449da9fc751f5cf4bb0
windowsazure <windowsazure>
2013-06-22 17:17:00 +0000

    Committing update from request https://votabl2.scm.azure-mobile.net/scmvfs/service/api/eventcount.js

---
+++ service/api/eventcount.js
@@ -0,0 +1,7 @@
+exports.post = function(request, response) {
+    // Use "request.service" to access features of your mobile service, e.g.:
+    //   var tables = request.service.tables;
+    //   var push = request.service.push;
+
+    response.send(200, "Hello World");
+};
