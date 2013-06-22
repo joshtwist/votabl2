c2d3abac9cd451971b3088f148610f35739eb6e7
windowsazure <windowsazure>
2013-06-22 17:15:58 +0000

    Committing delete from request https://votabl2.scm.azure-mobile.net/scmvfs/service/api/monitorevent.js

--- service/api/monitorevent.js
+++
@@ -1,8 +0,0 @@
-exports.post = function(request, response) {
-    // Use "request.service" to access features of your mobile service, e.g.:
-    //   var tables = request.service.tables;
-    //   var push = request.service.push;
-    console.log(request.body);
-    console.log(request.data);
-    response.send(200, [{ a : 1}, { b : 2} ]);
-};
