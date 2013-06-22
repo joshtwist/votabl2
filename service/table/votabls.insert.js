da940f98d4fb4fe920c455ece4a9bc78c116fab3
windowsazure <windowsazure>
2013-06-22 16:25:37 +0000

    Committing update from request https://votabl2.scm.azure-mobile.net/scmvfs/service/table/votabls.insert.js

---
+++ service/table/votabls.insert.js
@@ -0,0 +1,21 @@
+var _r = require;
+var require = function(path) {
+	try {  return _r(path);	}
+	catch (e) { return _r('../../app_data/config/scripts/table/' + path); }
+}
+
+var blobs = require('../shared/blobs.js');
+var uuid = require('../shared/node_modules/uuid');
+
+function insert(item, user, request) {
+    // todo - create an image URL for blob storage
+    var imgId = uuid.v4();
+    var imgSasUrl = blobs.generateUrl(imgId);
+    item.imageUrl = imgSasUrl.substring(0, imgSasUrl.indexOf('?'));
+    request.execute({
+        success : function(){
+            item.imageUrl = imgSasUrl;
+            request.respond();
+        }
+    });
+}
