0a587c9cf9d7efdc49e080a4601045b432361b29
unknown <josh@molmorg.com>
2013-06-22 17:06:25 -0700

    moved to query

--- service/api/eventcount.js
+++ service/api/eventcount.js
@@ -1,6 +1,6 @@
 exports.post = function(request, response) {
     var sql = "SELECT COUNT(id) AS 'total', votablId FROM votabl2.votes WHERE eventShareId = ? GROUP BY votablId"
-    request.service.mssql.query(sql, [request.parameters.eventShareId], {
+    request.service.mssql.query(sql, [request.query.eventShareId], {
         success: function (results) {
             response.send(200, results);
         }
