e0ed417eaa1ce586e3e0d5c550370d217b07db60
unknown <josh@molmorg.com>
2013-06-25 09:22:56 -0700

    added snippets and disposer

---
+++ client/votabl2/Votabl2/Common/Disposer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Votabl2.Common
+{
+    class Disposer
+    {
+    }
+}
